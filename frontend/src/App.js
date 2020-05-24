4139f293af58fabb84858fe80a7be29b06a9df3b
Frida Eriksson <Frida.Eriksson@silf.se>
2020-05-24 10:51:02 +0200

    set up routes in App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { LandingPage } from 'pages/LandingPage'
 import { SignUp } from 'pages/SignUp'
 import { SignIn } from 'pages/SignIn'
+import { SecretPage } from 'pages/SecretPage'
+
 
 
 export const App = () => {
   return (
-    <div>
-      <SignUp />
-      <SignIn />
-
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact>
+          <LandingPage />
+        </Route>
+        <Route path='/signup' exact>
+          <SignUp />
+        </Route>
+        <Route path='/signin' exact>
+          <SignIn />
+        </Route>
+        <Route path='/secretpage' exact>
+          <SecretPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
